645fd7915893ca547533f5c72776735a099224e7
Bharath Sridharan <bharath.sridharan@upstride.io>
2021-01-12 15:55:18 +0100

    updated based on review comments

--- bn_compare.py
+++ bn_compare.py
@@ -12,14 +12,14 @@ from bn_dcn_source import ComplexBatchNormalization
 
 tf.keras.backend.set_image_data_format('channels_first')
 
-class C_to_B(tf.keras.layers.Layer):
+class Channel2Batch(tf.keras.layers.Layer):
   # our BN implementation expects the inputs of multivector component in the Batch
   # Hence we split the inputs on the channel axis and concat them to the Batch 
   def call(self, inputs):
     x = tf.split(inputs, 2, axis=1)
     return tf.concat(x, axis=0)
     
-class B_to_C(tf.keras.layers.Layer):
+class Batch2Channel(tf.keras.layers.Layer):
   # We do the reverse. Split the inputs on the batch and concat them on the channel.
   def call(self, inputs):
     x = tf.split(inputs, 2, axis=0)
@@ -38,14 +38,13 @@ class TestBatchNorm(unittest.TestCase):
     }
     self.epochs = 20
     self.loss = tf.keras.losses.MeanSquaredError()
-    self.optimizer = tf.keras.optimizers.Adam(lr=0.0)
     self.input_shape = (6,3,3)
 
-  def get_dataset(self, l):
+  def get_dataset(self, list_of_batches):
     # gets the list of tensors, concats them on the batch
     # outputs tf.data.dataset object with specified batch size
-    X = tf.concat(l,axis=0)
-    y = tf.reshape(X, (self.batch_size * len(l) ,reduce(lambda x,y: x*y, self.input_shape)))
+    X = tf.concat(list_of_batches,axis=0)
+    y = tf.reshape(X, (self.batch_size * len(list_of_batches) ,reduce(lambda x,y: x*y, self.input_shape)))
     dataset = tf.data.Dataset.from_tensor_slices((X, y))
     dataset = dataset.batch(self.batch_size)
     return dataset
@@ -53,9 +52,9 @@ class TestBatchNorm(unittest.TestCase):
   def get_model(self,is_dcn_ours=True):
     inputs = tf.keras.layers.Input(shape=self.input_shape)
     if is_dcn_ours:
-      x = C_to_B()(inputs)
+      x = Channel2Batch()(inputs)
       x = layers.BatchNormalizationC(**self.bn_args)(x)
-      x = B_to_C()(x)
+      x = Batch2Channel()(x)
       x = layers.Flatten()(x)
     else:
       x = ComplexBatchNormalization(**self.bn_args)(inputs)
@@ -64,7 +63,7 @@ class TestBatchNorm(unittest.TestCase):
     model.summary()
     return model
 
-  def custom_training_loop(self,dataset,model):
+  def custom_training_loop(self,dataset,model,optimizer):
     weight_dict = defaultdict(list)
     for e in range(self.epochs):
       # print(f"Epoch number {e}/{self.epochs}")
@@ -73,29 +72,33 @@ class TestBatchNorm(unittest.TestCase):
           y_hat = model(x_batch, training=True)
           loss_value = self.loss(y_batch, y_hat)
         grads = tape.gradient(loss_value, model.trainable_weights)
-        self.optimizer.apply_gradients(zip(grads,model.trainable_weights))
+        optimizer.apply_gradients(zip(grads,model.trainable_weights))
         for weight in model.weights:
           weight_name = weight.name.split('/')[1]
           weight_dict[weight_name].append([e,step,weight.numpy()[0]]) #picking the first value from the weight (values ares same)
-    del grads, tape, model
     return weight_dict
 
-  def compare_runs(self,l):
-    dataset = self.get_dataset(l)
-    model_dcn_ours = self.get_model(is_dcn_ours=True)
-    weights_dcn_ours = self.custom_training_loop(dataset, model_dcn_ours)
+  def compare_runs(self,list_of_batches,optimizer=tf.keras.optimizers.Adam(lr=0.01)):
+    dataset = self.get_dataset(list_of_batches)
 
-    tf.keras.backend.clear_session()
-    # del self.get_model
     # Training for DCN ours
-    model_dcn_source = self.get_model(is_dcn_ours=False)
-    weights_dcn_source = self.custom_training_loop(dataset, model_dcn_source)
+    model_dcn_ours = self.get_model(is_dcn_ours=True)
+    weights_dcn_ours = self.custom_training_loop(dataset, model_dcn_ours,optimizer)
 
+    # clear the graph
+    tf.keras.backend.clear_session()
+    
+    # Training for DCN source
+    model_dcn_source = self.get_model(is_dcn_ours=False)
+    weights_dcn_source = self.custom_training_loop(dataset, model_dcn_source,optimizer)
+
+    # For the purpose of comparison these are removed from the DCN ours, DCN source adds weight for the whole input shape
     del weights_dcn_ours['betai:0']
     del weights_dcn_ours['moving_meani:0']
+
     print(weights_dcn_ours.keys()) 
     print(weights_dcn_source.keys())
-    for (k1, v1), (k2, v2) in zip(sorted(weights_dcn_ours.items(),key=lambda x: x), sorted(weights_dcn_source.items(),key=lambda x: x)):
+    for (k1, v1), (k2, v2) in zip(sorted(weights_dcn_ours.items()), sorted(weights_dcn_source.items())):
       # if k1 == k2:
         # print(k1,k2)
         # self.assertTrue(np.allclose(v1,v2))
@@ -104,15 +107,48 @@ class TestBatchNorm(unittest.TestCase):
       print(k1,v1[0])
       print(k2,v2[0])
 
-  def test_dataset_dcn_source(self):
-    l = []
+  def test_dataset_ones_lr_zero(self):
+    list_of_batches = []
     for i in range(1, 11): 
-      # l.append(tf.ones((16,6,3,3)) * i)
-      # # im = tf.ones((16,3,3,3)) * (2*i)
+      list_of_batches.append(tf.ones((self.batch_size,6,3,3)) * i)
+    self.compare_runs(list_of_batches,tf.keras.optimizers.Adam(lr=0.0))
+
+  def test_dataset_ones(self):
+    list_of_batches = []
+    for i in range(1, 11): 
+      list_of_batches.append(tf.ones((self.batch_size,6,3,3)) * i)
+    self.compare_runs(list_of_batches)
+    
+  def test_dataset_ones_2i(self):
+    list_of_batches = []
+    for i in range(1, 11): 
+      list_of_batches.append(tf.ones((self.batch_size,6,3,3)) * (2*i))
+    self.compare_runs(list_of_batches)
+
+  def test_dataset_r_ones_i_zeros(self):
+    list_of_batches = []
+    for i in range(1, 11): 
+      re = tf.ones((self.batch_size,3,3,3))
+      im = tf.zeros((self.batch_size,3,3,3))
+      list_of_batches.append(tf.concat([re,im], axis=1))
+    self.compare_runs(list_of_batches)
+
+  def test_dataset_r_i_random_normal(self):
+    list_of_batches = []
+    for i in range(1, 11): 
       re = tf.random.normal((self.batch_size,3,3,3), seed=42)
-      im = tf.random.normal((self.batch_size,3,3,3), seed=42, mean=1. ,stddev=2.)
-      l.append(tf.concat([re,im], axis=1))
-    self.compare_runs(l)
+      im = tf.random.normal((self.batch_size,3,3,3), seed=42)
+      list_of_batches.append(tf.concat([re,im], axis=1))
+    self.compare_runs(list_of_batches)
+
+  def test_dataset_r_i_random_normal_mean_stddev(self):
+    list_of_batches = []
+    for i in range(1, 11): 
+      re = tf.random.normal((self.batch_size,3,3,3), seed=42)
+      im = tf.random.normal((self.batch_size,3,3,3), seed=42, mean=1., stddev=2.)
+      list_of_batches.append(tf.concat([re,im], axis=1))
+    self.compare_runs(list_of_batches)
+
     # FIXME which variable is causing to cause differences in the gamma and beta to stay and mess with 2 runs
 
     # os.makedirs("DCN_ours",exist_ok=True)
