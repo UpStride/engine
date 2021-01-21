de2a9d0a2cdc23e7a07053d9dd94d93a641c8d88
Sebastien Iooss <archimist.linux@gmail.com>
2021-01-21 13:43:39 +0100

    2.0.0a2

--- create_and_push_docker.sh
+++ create_and_push_docker.sh
@@ -8,10 +8,10 @@
 
 if [[ ${1,,} == "cython" ]]; then 
     echo "Building docker with Cython compiled Python Engine"
-    docker build -t eu.gcr.io/fluid-door-230710/upstride:py-`cat dist_binary_setup.py | grep version|cut -f2 -d'"'`-tf2.3.0-gpu-cython-compiled -f dist_binary.dockerfile .
-    docker push eu.gcr.io/fluid-door-230710/upstride:py-`cat dist_binary_setup.py | grep version|cut -f2 -d'"'`-tf2.3.0-gpu-cython-compiled 
+    docker build -t eu.gcr.io/fluid-door-230710/upstride:py-`cat dist_binary_setup.py | grep version|cut -f2 -d'"'`-tf2.4.0-gpu-cython-compiled -f dist_binary.dockerfile .
+    docker push eu.gcr.io/fluid-door-230710/upstride:py-`cat dist_binary_setup.py | grep version|cut -f2 -d'"'`-tf2.4.0-gpu-cython-compiled 
 else
     echo "Building docker with Python Engine"
-    docker build -t eu.gcr.io/fluid-door-230710/upstride:py-`cat setup.py|grep version|cut -f2 -d'"'`-tf2.3.0-gpu .
-    docker push eu.gcr.io/fluid-door-230710/upstride:py-`cat setup.py|grep version|cut -f2 -d'"'`-tf2.3.0-gpu
+    docker build -t eu.gcr.io/fluid-door-230710/upstride:py-`cat setup.py|grep version|cut -f2 -d'"'`-tf2.4.0-gpu .
+    docker push eu.gcr.io/fluid-door-230710/upstride:py-`cat setup.py|grep version|cut -f2 -d'"'`-tf2.4.0-gpu
 fi
