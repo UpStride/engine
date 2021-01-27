4be93f69a317605bc8dad3dbabf157299a07415c
Mohammad Reza Loghmani <mrloghmani@upstride.io>
2021-01-27 18:22:12 +0000

    Merged in linebound_activation (pull request #20)
    Linebound activation
    
    * update complex activationsand test: remove useless aold ones, add linebound
    
    * Update dockerfile to pull TF2.4

--- upstride/type1/tf/keras/test_activations.py
+++ upstride/type1/tf/keras/test_activations.py
@@ -3,7 +3,8 @@ import random
 import tensorflow as tf
 import numpy as np
 from math import pi
-from .activations import cos_fn, cos_fn_grad, pow2_fn, pow2_fn_grad
+from .activations import relu_linebound_fn, swish_linebound_fn
+from tensorflow.math import sin, cos, atan2, pow, sqrt, sigmoid, greater_equal, multiply
 
 def get_tf_grad(z, fn, *args):
   a, b = tf.constant(z[0]), tf.constant(z[1])
@@ -14,111 +15,110 @@ def get_tf_grad(z, fn, *args):
     dy_da, dy_db = g.gradient(y, a), g.gradient(y, b)
 
   return [dy_da, dy_db]
-  
 
-class TestActivationCos(unittest.TestCase):
+def NHWC_relu_linebound_fn(z, l):
+  """
+  Former version of the linebound function implemented for the old engine => channel last format.
+  Use this function to make sure we get the same results with the channel first implementation.
+  """
 
-  def test_forward_scalar(self):
-    a, b = 0.0, 0.0
-    target_a, target_b = 1, 0
-    out = cos_fn([a,b])
-    self.assertTrue(np.array_equal(out[0].numpy(),target_a) and np.array_equal(out[1].numpy(),target_b))
+  z = tf.transpose(z, [0, 2, 3, 1])
+  a, b = tf.split(z, 2, axis=0)
 
-  def test_forward_zeros(self):
-    a, b = np.zeros([2,3,4]), np.zeros([2,3,4])
-    target_a, target_b = np.ones([2,3,4]), np.zeros([2,3,4])
-    out = cos_fn([a,b])
-    self.assertTrue(np.array_equal(out[0].numpy(),target_a) and np.array_equal(out[1].numpy(),target_b))
+  ## Original function
+  # START
+  #a, b = z[0], z[1]
+  a = tf.transpose(a, [0, 3, 1, 2])
+  b = tf.transpose(b, [0, 3, 1, 2])
 
-  def test_forward_90deg(self):
-    a, b = np.ones([2,3,4])*(pi/2), np.zeros([2,3,4])
-    target_a, target_b = np.ones([2,3,4])*(pi/2), np.zeros([2,3,4])
-    out = cos_fn([a,b])
-    self.assertTrue(np.array_equal(out[0].numpy(),target_a) and np.array_equal(out[1].numpy(),target_b))
+  z_new = tf.concat([tf.expand_dims(a,axis=0), tf.expand_dims(b,axis=0)], axis=0)
+  lam = tf.tensordot(l, z_new, axes=1)
+  condition = greater_equal(lam, 0)
 
-  def test_backward_zeros(self):
-    a, b = np.zeros([2,3,4]), np.zeros([2,3,4])
-    target_a, target_b = np.ones([2,3,4]), np.ones([2,3,4])
+  real = tf.where(condition, tf.tanh(lam)*a, 0)
+  imag = tf.where(condition, tf.tanh(lam)*b, 0)
+  real = tf.transpose(real, [0, 2, 3, 1])
+  imag = tf.transpose(imag, [0, 2, 3, 1])
+  # END
 
-    ## Manual gradients
-    target_da, target_db = cos_fn_grad([a,b])
+  return tf.transpose(tf.concat([real, imag], axis=0), [0, 3, 1, 2])
 
-    ## TF computed gradient
-    dy_da, dy_db = get_tf_grad([a,b], cos_fn)
+def NHWC_swish_linebound_fn(z, l):
+  """
+  Former version of the linebound function implemented for the old engine => channel last format.
+  Use this function to make sure we get the same results with the channel first implementation.
+  """
 
-    self.assertTrue(np.array_equal(dy_da,target_da) and np.array_equal(dy_db ,target_db) \
-    and np.array_equal(dy_da,target_a) and np.array_equal(dy_db,target_b))
+  z = tf.transpose(z, [0, 2, 3, 1])
+  a, b = tf.split(z, 2, axis=0)
 
-  def test_backward_90deg(self):
-    a, b = np.ones([2,3,4])*(pi/2), np.zeros([2,3,4])
-    target_a, target_b = np.zeros([2,3,4]), np.zeros([2,3,4])
+  ## Original function
+  # START
+  #a, b = z[0], z[1]
+  a = tf.transpose(a, [0, 3, 1, 2])
+  b = tf.transpose(b, [0, 3, 1, 2])
+  phase = atan2(b,a)
+  norm = sqrt(pow(a,2)+pow(b,2))
 
-    ## Manual gradients
-    target_da, target_db = cos_fn_grad([a,b])
+  z_new = tf.concat([tf.expand_dims(a,axis=0), tf.expand_dims(b,axis=0)], axis=0)
+  lam = tf.tensordot(l, z_new, axes=1)
+  condition = greater_equal(lam, 0)
 
-    ## TF computed gradient
-    dy_da, dy_db = get_tf_grad([a,b], cos_fn)
+  smooth_norm = tf.nn.sigmoid(lam)*norm
+  smooth_phase = tf.nn.sigmoid(lam)*phase
 
-    self.assertTrue(np.array_equal(dy_da,target_da) and np.array_equal(dy_db ,target_db) \
-    and np.array_equal(dy_da,target_a) and np.array_equal(dy_db ,target_b))
+  real = multiply(smooth_norm,cos(smooth_phase))
+  imag = multiply(smooth_norm,sin(smooth_phase))
+  real = tf.transpose(real, [0, 2, 3, 1])
+  imag = tf.transpose(imag, [0, 2, 3, 1])
+
+  # END
+
+  return tf.transpose(tf.concat([real, imag], axis=0), [0, 3, 1, 2])
 
 
-class TestActivationPow2(unittest.TestCase):
+class TestLineBound(unittest.TestCase):
 
-  def test_forward_scalar(self):
-    a, b = 0.0, 0.0
-    target_a, target_b = 0.0, 0.0
-    out = pow2_fn([a,b])
-    self.assertTrue(np.array_equal(out[0].numpy(),target_a) and np.array_equal(out[1].numpy(),target_b))
+  def test_relu_forward_negative(self):
+    z = tf.constant((-1000)*np.ones([4,4,4,4], dtype=np.float32))
+    l = tf.constant([1.,1.])
+    target = np.zeros([4,4,4,4], dtype=np.float32)
+    out = relu_linebound_fn(z,l)
+    self.assertTrue(np.allclose(out.numpy(),target, rtol=1e-03, atol=1e-03))
 
-  def test_forward_zeros(self):
-    a, b = np.zeros([2,3,4]), np.zeros([2,3,4])
-    target_a, target_b = np.zeros([2,3,4]), np.zeros([2,3,4])
-    out = pow2_fn([a,b])
-    self.assertTrue(np.array_equal(out[0].numpy(),target_a) and np.array_equal(out[1].numpy(),target_b))
+  def test_relu_forward_positive(self):
+    z = tf.constant((+1000)*np.ones([4,4,4,4], dtype=np.float32))
+    l = tf.constant([1.,1.])
+    target = (+1000)*np.ones([4,4,4,4], dtype=np.float32)
+    out = relu_linebound_fn(z,l)
+    self.assertTrue(np.allclose(out.numpy(),target, rtol=1e-03, atol=1e-03))
 
-  def test_forward_ones(self):
-    a, b = np.ones([2,3,4]), np.ones([2,3,4])
-    target_a, target_b = np.zeros([2,3,4]), 2*np.ones([2,3,4])
-    out = pow2_fn([a,b])
-    self.assertTrue(np.array_equal(out[0].numpy(),target_a) and np.array_equal(out[1].numpy(),target_b))
+  def test_relu_old_version(self):
+    l = tf.constant([1.,1.])
+    for i in range(10):
+      z = tf.random.uniform([4,4,4,4], seed=i)
+      target = NHWC_relu_linebound_fn(z,l)
+      out = relu_linebound_fn(z,l)
+      self.assertTrue(np.allclose(out.numpy(),target))
 
-  def test_backward_zeros(self):
-    a, b = np.zeros([2,3,4]), np.zeros([2,3,4])
-    target_a, target_b = np.zeros([2,3,4]), np.zeros([2,3,4])
-    
-    ## Manual gradients
-    target_da, target_db = pow2_fn_grad([a,b])
+  def test_swish_forward_negative(self):
+    z = tf.constant((-1000)*np.ones([4,4,4,4], dtype=np.float32))
+    l = tf.constant([1.,1.])
+    target = np.zeros([4,4,4,4], dtype=np.float32)
+    out = swish_linebound_fn(z,l)
+    self.assertTrue(np.allclose(out.numpy(),target, rtol=1e-03, atol=1e-03))
 
-    ## TF computed gradient
-    dy_da, dy_db = get_tf_grad([a,b], pow2_fn)
+  def test_swish_forward_positive(self):
+    z = tf.constant((+1000)*np.ones([4,4,4,4], dtype=np.float32))
+    l = tf.constant([1.,1.])
+    target = (+1000)*np.ones([4,4,4,4], dtype=np.float32)
+    out = swish_linebound_fn(z,l)
+    self.assertTrue(np.allclose(out.numpy(),target, rtol=1e-03, atol=1e-03))
 
-    self.assertTrue(np.array_equal(dy_da, target_da) and np.array_equal(dy_db ,target_db))
-    self.assertTrue(np.array_equal(dy_da, target_a) and np.array_equal(dy_db ,target_b))
-
-  def test_backward_ones(self):
-    a, b = np.ones([2,3,4]), np.ones([2,3,4])
-    target_a, target_b = 4*np.ones([2,3,4]), np.zeros([2,3,4])
-    ## Manual gradients
-    target_da, target_db = pow2_fn_grad([a,b])
-
-    ## TF computed gradient
-    dy_da, dy_db = get_tf_grad([a,b], pow2_fn)
-
-    self.assertTrue(np.array_equal(dy_da, target_da) and np.array_equal(dy_db ,target_db))
-    self.assertTrue(np.array_equal(dy_da, target_a) and np.array_equal(dy_db ,target_b))
-
-  def test_backward_alphas(self):
-    a, b = np.ones([2,3,4]), np.ones([2,3,4])
-
-    for _ in range(10):
-
-      alpha = random.uniform(0.5,5.0)
-
-      ## Manual gradients
-      target_da, target_db = pow2_fn_grad([a,b], alpha)
-
-      ## TF computed gradient
-      dy_da, dy_db = get_tf_grad([a,b], pow2_fn, alpha)
-
-      self.assertTrue(np.array_equal(dy_da, target_da) and np.array_equal(dy_db ,target_db))
+  def test_swish_old_version(self):
+    l = tf.constant([1.,1.])
+    for i in range(10):
+      z = tf.random.uniform([4,4,4,4], seed=i)
+      target = NHWC_swish_linebound_fn(z,l)
+      out = swish_linebound_fn(z,l)
+      self.assertTrue(np.allclose(out.numpy(),target))
