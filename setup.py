3048dc1298742603ba66277788ada11ab4bcfb71
Sebastien Iooss <archimist.linux@gmail.com>
2021-04-21 15:28:36 +0200

    clean setup.py and dockerfile

--- setup.py
+++ setup.py
@@ -1,26 +1,23 @@
 import setuptools
-import codecs
 
 with open('version') as f:
   version = f.readline()
+
+with open("README.md", "r") as f:
+  long_description = f.read()
 
 setuptools.setup(
     name="upstride",
     version=version,
     author="UpStride S.A.S",
     author_email="hello@upstride.io",
-    description="",
-    long_description='',
+    description="Upstride computation engine for Geometrical Algebra neural network",
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://upstride.io",
     packages=setuptools.find_packages(),
-    include_package_data=True,
-    package_data={'': ['_upstride_ops.so']},
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
-    ],
-    # test_suite="",
-    # tests_require=[''],
-    install_requires=['']
+    ]
 )
